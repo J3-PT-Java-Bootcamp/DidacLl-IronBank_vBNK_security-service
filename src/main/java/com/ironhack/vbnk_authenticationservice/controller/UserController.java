0880024d899549dda953274d8f19c839c66a67ba
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-10 18:49:37 +0200

    KeyCloak implementation

--- src/main/java/com/ironhack/vbnk_authenticationservice/controller/UserController.java
+++ src/main/java/com/ironhack/vbnk_authenticationservice/controller/UserController.java
@@ -19,7 +19,7 @@ import javax.ws.rs.BadRequestException;
 import javax.ws.rs.core.Response;
 
 @RestController
-@RequestMapping("v1/security")
+@RequestMapping("/v1/security")
 public class UserController {
     private final KeycloakAdminClientService kcAdminClient;
 
@@ -32,14 +32,14 @@ public class UserController {
     }
 
 
-    @PostMapping(value = "admin/create")
+    @PostMapping(value = "/auth/create")
     public ResponseEntity<?> createUser(@RequestBody CreateUserRequest user) {
         Response createdResponse = kcAdminClient.createKeycloakUser(user);
         return ResponseEntity.status(createdResponse.getStatus()).build();
 
     }
 
-    @PostMapping("public/token")
+    @PostMapping("/public/token")
     public ResponseEntity<AccessTokenResponse> login(@NotNull @RequestBody LoginRequest loginRequest) {
         Keycloak keycloak = kcProvider.newKeycloakBuilderWithPasswordCredentials(loginRequest.getUsername(), loginRequest.getPassword()).build();
 
